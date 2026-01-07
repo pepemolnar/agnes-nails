c4be4ffbef888936af9e069475c720c870fedbb8
pepemolnar <50260402+pepemolnar@users.noreply.github.com>
2026-01-07 10:20:24 +0100

    Delete backend directory

--- backend/src/blocked-dates/dto/update-blocked-date.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBlockedDateDto } from './create-blocked-date.dto';
-
-export class UpdateBlockedDateDto extends PartialType(CreateBlockedDateDto) {}
--- backend/src/open-hours/dto/update-open-hour.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateOpenHourDto } from './create-open-hour.dto';
-
-export class UpdateOpenHourDto extends PartialType(CreateOpenHourDto) {}
